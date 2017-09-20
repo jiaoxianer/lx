bb68de6da871029d6b814a024fba521fa1dc6ba7
tom <tom@itcast>
2017-09-20 17:20:39 +0800

    初始化退出功能v2.0.0

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,34 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+	//实现退出功能
+	$('#logoutBtn').click(function(){
+		$.ajax({
+			type:'post',
+			url:'/api/logout',
+			dataType:'json',
+			success:function(data){
+				//console.log(data)
+				if(data.code == 200){
+					//跳转到登录页
+					location.href = '/main/login';
+				}
+			}
+		})
+	});
+
+	//检测用户是否登录
+	var flag = $.cookie('PHPSESSID');
+	//这个属性是为了阻止在判断的时候PHPSESSID不存在，一直在刷新页面，加上location.pathname != '/main/login' 的时候
+	//console.log(location.pathname);
+	if(!flag){
+		//如果cookie不存在跳转到登录页
+		location.href = '/main/login';
+	}
+
+	 //设置用户头像信息
+	 //console.log($.cookie('loginInfo'));
+	 var loginInfo = $.cookie('loginInfo');
+   	 loginInfo = loginInfo && JSON.parse(loginInfo);
+	 $('.aside .profile img').attr('src',loginInfo.tc_avatar);
+     $('.aside .profile h4').html(loginInfo.tc_name);
