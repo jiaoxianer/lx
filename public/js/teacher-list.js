dc1d442f6d97edeb984a576d0be2d9166e506840
tom <tom@itcast>
2017-09-27 09:52:33 +0800

    完成讲师预览功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,11 +1,11 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
 	//调用接口获取所有讲师数据
 	$.ajax({
 		type:'get',
 		url:'/api/teacher',
 		dataType:'json',
 		success:function(data){
-			console.log(data);
+			//console.log(data);
 			//解析数据，渲染页面
 			var html = template('teacherTpl',{list:data.result});
 			$('#teacherInfo').html(html);
@@ -14,13 +14,13 @@ define(['jquery','template'],function($,template){
 				var that = this;
 				//获取父元素从而得到tcId  tc_status 
 				var td = $(this).closest('td');
-				var tcId = td.attr('data-tcId');
+				var tcID = td.attr('data-tcID');
 				var status = td.attr('data-status');
 				//console.log(tcId,status)
 				$.ajax({
 					type : 'post',
 					url : '/api/teacher/handle',
-					data : {tc_id : tcId,tc_status : status},
+					data : {tc_id : tcID,tc_status : status},
 					dataType :'json',
 					success :function(data){
 						if(data.code == 200){
@@ -34,6 +34,24 @@ define(['jquery','template'],function($,template){
 					}
 				});
 			});
+			//查看预览讲师功能
+			$('.preview').click(function(){
+				var td = $(this).closest('td');
+				var tcID = td.attr('data-tcID'); 
+				$.ajax({
+					type : 'get',
+					url : '/api/teacher/view',
+					data : {tc_id : tcID},
+					dataType : 'json',
+					success :function(data){
+						//console.log(data);
+						var html = template('modalTpl',data.result);
+						$('#modalInfo').html(html);
+						$('#teacherModal').modal();
+					}
+				});
+			})
+			
 
 		}
 	});
