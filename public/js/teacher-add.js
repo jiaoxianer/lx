b59f681b85166d78ba44eb6348ece8e3d3274e3e
tom <tom@itcast>
2017-09-27 12:29:24 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -4,24 +4,44 @@ define(['jquery','template','util'],function($,template,util){
 	 var tcId = util.qs('tc_id');
     //console.log(tcId)
     if(tcId){
-    	//编辑页面操作,需要根据tc_id 获取数据渲染页面
-    	$.ajax({
-    		type : 'get',
-    		url : '/api/teacher/edit',
-    		data : {tc_id : tcId},
-    		dataType : 'json',
-    		success : function(data){
-    			//console.log(data);
-    			//解析数据渲染页面
-          		data.result.operate = '编辑讲师';
-    			var html =template('teacherTpl',data.result);
-    			$('#teacherInfo').html(html);
-
-    		}
-    	});
+      //编辑操作
+      $.ajax({
+        type:'get',
+        url :'/api/teacher/edit',
+        data:{tc_id : tcId },
+        dataType:'json',
+        success:function(data){
+          //解析数据渲染页面
+          data.result.operate = '编辑讲师';
+          var html = template('teacherTpl',data.result);
+          $('#teacherInfo').html(html);
+          //处理表单提交
+          submitForm('/api/teacher/update');
+        }
+      })
     }else{
-    	//添加页面操作
-    	var html =template('teacherTpl',{operate:'添加讲师'});
-    	$('#teacherInfo').html(html);
+      //添加操作
+      var html = template('teacherTpl',{operate:'添加讲师'});
+      $('#teacherInfo').html(html);
+      submitForm('/api/teacher/add');
     }
+
+    //实现表单提交
+   function submitForm(url){
+      $('#teacherBtn').click(function(){
+      	console.log($('#teacherForm').serialize())
+        $.ajax({
+          type:'post',
+          url : url,
+          data:$('#teacherForm').serialize(),
+          dataType:'json',
+          success:function(data){
+          	console.log(1)
+            if(data.code == 200){
+              location.href = '/teacher/list';
+            }
+          }
+        });
+      });
+    };
 });
